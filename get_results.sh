c9cbba1050361b65c4b909ae0972f0f229f42f59
Junwon Park <junwonpk@stanford.edu>
2018-11-15 14:21:08 -0800

    Version 2

--- get_results.sh
+++ get_results.sh
@@ -1,3 +1,19 @@
 python get_results.py \
+  --hit_ids_file=examples/image_sentence/technique0hits.txt \
+  > examples/image_sentence/technique0results.txt --prod
+
+python get_results.py \
   --hit_ids_file=examples/image_sentence/technique1hits.txt \
-  > examples/image_sentence/hit1results.txt --prod
+  > examples/image_sentence/technique1results.txt --prod
+
+python get_results.py \
+  --hit_ids_file=examples/image_sentence/technique2hits.txt \
+  > examples/image_sentence/technique2results.txt --prod
+
+python get_results.py \
+  --hit_ids_file=examples/image_sentence/technique3hits.txt \
+  > examples/image_sentence/technique3results.txt --prod
+
+python get_results.py \
+  --hit_ids_file=examples/image_sentence/technique4hits.txt \
+  > examples/image_sentence/technique4results.txt --prod
---
+++ reject_hits.sh
@@ -0,0 +1,1 @@
+python reject_hits.py --hit_ids_file=examples/image_sentence/reject_hits.txt --prod
