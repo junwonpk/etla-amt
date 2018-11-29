c33556c4e4e04fd8f0515cd68faca28c18a266c3
Junwon Park <junwon@orbis.ai>
2018-11-28 22:06:29 -0800

    10k Images

--- get_results.sh
+++ get_results.sh
@@ -1,19 +1,3 @@
 python get_results.py \
-  --hit_ids_file=examples/image_sentence/technique0hits.txt \
-  > examples/image_sentence/technique0results.txt --prod
-
-python get_results.py \
-  --hit_ids_file=examples/image_sentence/technique1hits.txt \
-  > examples/image_sentence/technique1results.txt --prod
-
-python get_results.py \
-  --hit_ids_file=examples/image_sentence/technique2hits.txt \
-  > examples/image_sentence/technique2results.txt --prod
-
-python get_results.py \
-  --hit_ids_file=examples/image_sentence/technique3hits.txt \
-  > examples/image_sentence/technique3results.txt --prod
-
-python get_results.py \
-  --hit_ids_file=examples/image_sentence/technique4hits.txt \
-  > examples/image_sentence/technique4results.txt --prod
+  --hit_ids_file=examples/image_sentence/hit_ids.txt \
+  > examples/image_sentence/results.txt
